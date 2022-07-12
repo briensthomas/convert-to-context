87c1b829036deb38140511f7db2b4798503dcc51
Brien Thomas <thomasbrien94@gmail.com>
2022-07-11 17:00:15 -0700

    forgetting the parentheses on useGameContext() causes things to break

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CardGameProvider from './CardGameProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CardGameProvider>
+      <App />
+    </CardGameProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
